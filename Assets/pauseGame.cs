822e47d190c767426b549ac5be3ea8d3192d8008
Ryan Maher <maherrt@sunypoly.edu>
2020-11-12 13:16:31 -0500

    New basic Pause menu
    I'm making a new branch so this doesnt brick again :)

---
+++ Assets/pauseGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseGame : MonoBehaviour
+{
+    public void doPause()
+    {
+        SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
+    }
+}
